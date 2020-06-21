345c9289a80d8e4dd4b0865717f6970430bc03e2
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-21 21:18:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ const [red, orange, yellow, green, blue, indigo, violet] = colors
 const [r, o, y, g, b, , v] = colors
 
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+
+const [, , , , , indg, ] = colors
 
 // Objects
 
